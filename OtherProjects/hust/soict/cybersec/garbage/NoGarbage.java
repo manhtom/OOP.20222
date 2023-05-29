02ad387ffb8b234da580bbc10168ff8033d78d18
D <manhtom2003@gmail.com>
2023-05-29 14:37:25 +0700

    Squashed commit of the following:
    commit d2e1ddc6693d25cb0dcd1249912269330aeecf5f
    Merge: d0248cb 3ca4e45
    Author: D <manhtom2003@gmail.com>
    Date:   Mon May 29 14:12:48 2023 +0700
    
        Merge branch 'topic/memory-management-string' of https://github.com/manhtom/OOP.20222 into topic/memory-management-string
    
    commit d0248cbed416e7c865253d189edf7c156092fb2c
    Author: D <manhtom2003@gmail.com>
    Date:   Mon May 29 14:12:35 2023 +0700
    
        various modification after testing
    
    commit 3ca4e45cf401b0663f1af74ea8b3e73bb886cf85
    Author: D <manhtom2003@gmail.com>
    Date:   Mon May 29 14:10:56 2023 +0700
    
        StringBuilder -> StringBuffer
    
    commit afb0211de2956a1392bba055324a7602b42460d4
    Author: D <manhtom2003@gmail.com>
    Date:   Mon May 29 14:05:53 2023 +0700
    
        Update README.md
    
    commit 31c2f2d94bd9ea65b1e3b1b2576852206a96177a
    Author: D <manhtom2003@gmail.com>
    Date:   Mon May 29 14:04:45 2023 +0700
    
        Create README.md
    
    commit 933b5dc0d06ebfb47c73ea0b06af2c2f4ccd37fd
    Author: D <manhtom2003@gmail.com>
    Date:   Mon May 29 13:27:46 2023 +0700
    
        initial implementation
    
    commit ccdae4f265fb619b801a8cfe83505ffaf0290820
    Author: D <manhtom2003@gmail.com>
    Date:   Mon May 29 13:03:16 2023 +0700
    
        here's to the crazy ones text

--- OtherProjects/hust/soict/cybersec/garbage/NoGarbage.java
+++ OtherProjects/hust/soict/cybersec/garbage/NoGarbage.java
@@ -1,3 +1,30 @@
+// test text file contains nearly 100k words. No problem for StringBuffer...
+
+package garbage;
+
+import java.io.IOException;
+import java.nio.file.*;
+
 public class NoGarbage {
-    
+    public static void main(String[] args) {
+        String filename = "test.txt"; // test.txt is the name/path to a text file
+        byte[] inputBytes= {0};
+        long startTime, endTime;
+
+        try {
+            inputBytes = Files.readAllBytes(Paths.get(filename));
+            startTime= System.currentTimeMillis();
+            StringBuffer outputStringBuilder = new StringBuffer();
+            for (byte b : inputBytes) {
+                outputStringBuilder.append((char)b);
+            }
+            endTime=System.currentTimeMillis();
+            System.out.println("Processing time for StringBuffer: "+ (endTime-startTime)+"ms");
+        }
+
+        catch (IOException e) {
+            System.out.println("File not found. Ensure the path is correct.");
+        }
+
+    }
 }
