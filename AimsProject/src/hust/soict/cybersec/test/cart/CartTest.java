401a48dadf9792629a6e1148bfbce26fea50ab65
D <manhtom2003@gmail.com>
2023-05-29 20:27:14 +0700

    code presentation fix

--- AimsProject/src/hust/soict/cybersec/test/cart/CartTest.java
+++ AimsProject/src/hust/soict/cybersec/test/cart/CartTest.java
@@ -1,8 +1,8 @@
 public class CartTest {
     public static void main(String[] args) {
-		Cart anOrder = new Cart();
+        Cart anOrder = new Cart();
 		
-		// create new dvd objects and add them to the cart
+        // create new dvd objects and add them to the cart
         DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King","Animation","Roger Allers",87,19.95f);
         DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars","Science Fiction","George Lucas",87,24.95f);
         DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladdin","Animation",18.99f);
