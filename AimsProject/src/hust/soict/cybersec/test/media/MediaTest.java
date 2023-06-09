a9e10d5e05ca00695b250d4d9a7846759ef82858
D <manhtom2003@gmail.com>
2023-06-09 22:39:44 +0700

    MediaTest small fix

--- AimsProject/src/hust/soict/cybersec/test/media/MediaTest.java
+++ AimsProject/src/hust/soict/cybersec/test/media/MediaTest.java
@@ -13,12 +13,12 @@ public class MediaTest {
         // for example: cd, dvd, book
         CompactDisc cd = new CompactDisc("Ninth Symphony", "Classical", 14.00f, "Beethoven");
         DigitalVideoDisc dvd = new DigitalVideoDisc("The Lion King","Animation","Roger Allers",87,19.95f);
-        /Book book = new Book("Gone with the Wind", "Fiction" 19.88f, "Margaret Mitchell");
+        Book book = new Book("Gone with the Wind", "Fiction", 19.88f, "Margaret Mitchell");
         mediae.add(cd);
         mediae.add(dvd);
         mediae.add(book);
         for (Media m : mediae) {
-            m.toString();
+            System.out.println(m.toString());
         }
     }
 }
